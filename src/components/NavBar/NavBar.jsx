9a73491a784e9b6cd9a6ad1774b7d73cfb836fe7
Mike Liu <mliu8m@gmail.com>
2020-03-14 20:21:05 +0800

    updated username alert /signup

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router';
 
 const NavBar = () => {
