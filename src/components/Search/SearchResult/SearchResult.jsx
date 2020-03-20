defe3b0afb779e50c3f68c9a94ac0e3c3a070ace
Mike Liu <mliu8m@gmail.com>
2020-03-20 22:50:57 +0800

    wip on result list

--- src/components/Search/SearchResult/SearchResult.jsx
+++ src/components/Search/SearchResult/SearchResult.jsx
@@ -3,35 +3,43 @@ import { ListGroup } from 'react-bootstrap';
 
 const SearchResult = (props) => {
 
+  //wip on mapping result list
+  /* e.g. [
+    { brand: kodak, film: [a, b, c ] },
+    { brand: lomo, film: [a, b ] }
+  ]
+  */
+
+
   return(
-    <ListGroup className='searchResult'>
+    <div className='searchResult resultHeadColor'>
+      <ListGroup>
+        {
+          !props.results.length && (
+            <ListGroup.Item className='noResults'>
+              <p className="resultHead">No results found for query</p>
 
-      {
-        !props.results.length && (
-          <ListGroup.Item className='noResults'>
-            <p className="resultHead">No results found for query</p>
+              <div className='keywordBold'>
+                "{props.keyword && props.keyword.split('')}"
+              </div>
+            </ListGroup.Item>
+          )
+        }
 
-            <div className='keywordBold'>
-              "{props.keyword && props.keyword.split('')}"
-            </div>
-          </ListGroup.Item>
-        )
-      }
-
-      {
-        props.results.length > 0 && (
-          props.results.map((i, index) => {
-            return (
-              <ListGroup.Item key={index} action href={`/film/${i.seo.slug}`}>
-                { i.name }
-              </ListGroup.Item>
-            )
-            
-          })
-        )
-      }
-
-    </ListGroup>
+        {
+          props.results.length > 0 && (
+            props.results.map((i, index) => {
+              return (
+                <ListGroup.Item key={index} action href={`/film/${i.seo.slug}`}>
+                  { i.name }
+                </ListGroup.Item>
+              )
+              
+            })
+          )
+        }
+      </ListGroup>
+    </div>
   )
 };
 
