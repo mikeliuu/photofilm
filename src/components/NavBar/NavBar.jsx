be048819ab730038ebcad2b341187000abef91f6
Mike Liu <mliu8m@gmail.com>
2020-04-04 15:37:43 +0800

    updated mobile nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,6 +5,8 @@ import Search from '../Search/Search';
 import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
 import BookmarkIcon from "@material-ui/icons/Bookmark";
 import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
+
 
 const NavBar = () => {
   const { pathname } = useLocation();
@@ -34,63 +36,66 @@ const NavBar = () => {
   return (
     <nav className="navWrapper">
 
-      {/* desktop nav */}
-      <div className="navbar">
-        <Link to="/" className="navLogo">
-          Photofilm
-        </Link>
-
-        <div className='navRight'>
-          <Search />
-          <div className="navList">
+    {
+      state.isOpenDropdown ? (
+        <div className='mobileNav'>
+          <Search className='mobileSearch'/>
+          <div className='linkWrapper'>
             {
               navList && (
-                navList.map((n, index) => (
-                  <Link to={ n.path } className='navLink' key={index}>
-                    <div className={ pathname === n.path ? linkActive : linkNormal }>
-                      { n.text }
-                    </div>
-                  </Link>
-                ))
+                navList.map((n, index) => {
+                  // if(!n.icon) return;
+
+                  return(
+                    <Link to={ n.path } className='dropdownLink' key={index}>
+                      <div className={ pathname === n.path ? linkActive : linkNormal }>
+                        { n.text }
+                      </div>
+                    </Link>
+                  )
+                })
               )
             }
           </div>
+
+          <CloseRoundedIcon className='navCloseIcon' onClick={ () => onClickDropdown() }/>
         </div>
+      ) : (
+        <>
+          <div className="navbar">
+            <Link to="/" className="navLogo">
+              Photofilm
+            </Link>
 
-        <MoreHorizRoundedIcon className='navMenuIcon pinGrey' onClick={ () => onClickDropdown() }/>
-      </div>
-
-    {/* mobile nav */}
-      {
-        state.isOpenDropdown && (
-          <div className='mobileNav'>
-            <Search className='mobileSearch'/>
-            <div className='linkWrapper'>
-              {
-                navList && (
-                  navList.map((n, index) => {
-                    // if(!n.icon) return;
-
-                    return(
-                      <Link to={ n.path } className='dropdownLink' key={index}>
+            <div className='navRight'>
+              <Search />
+              <div className="navList">
+                {
+                  navList && (
+                    navList.map((n, index) => (
+                      <Link to={ n.path } className='navLink' key={index}>
                         <div className={ pathname === n.path ? linkActive : linkNormal }>
                           { n.text }
                         </div>
                       </Link>
-                    )
-                  })
-                )
-              }
+                    ))
+                  )
+                }
+              </div>
             </div>
+
+            <MoreHorizRoundedIcon className='navMenuIcon' onClick={ () => onClickDropdown() }/>
           </div>
+
+          <div id='navDivider'></div>
+        </>
         )
       }
 
-      <div id='navDivider'></div>
       
-      {
+      {/* {
         state.isOpenDropdown && <div className='navOverlay'></div>
-      }
+      } */}
 
     </nav>
   );
